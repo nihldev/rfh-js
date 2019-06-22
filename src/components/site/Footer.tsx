58c16ef35708be15ed165af20ef9a7826e2ffb65
Diego Romero <jd@inkatze.com>
2019-06-21 22:43:20 -0700

    Added components code

---
+++ src/components/site/Footer.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionalComponent } from "react";
+
+import "../assets/styles/components/Footer.css";
+
+const Footer: FunctionalComponent = () => {
+  return (
+    <footer>
+      <div>
+        <h4>Refaccionaria del Hogar</h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
