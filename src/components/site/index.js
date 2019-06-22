7a1c866c0670a606bb4d0f1c69bd1f3854a0fe0d
Diego Romero <jd@inkatze.com>
2019-06-21 23:06:24 -0700

    Added index files

---
+++ src/components/site/index.js
@@ -0,0 +1,4 @@
+export { default as Contact } from "./Contact";
+export { default as Content } from "./Content";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
